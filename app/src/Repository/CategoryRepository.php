7100f7c0c0cd88543e8761f2f06a8431c756d0c5
4k-Komp <you@example.com>
2023-05-16 15:22:01 +0200

    symfony category service

--- app/src/Repository/CategoryRepository.php
+++ app/src/Repository/CategoryRepository.php
@@ -1,19 +1,23 @@
 <?php
-
+/**
+ * Category repository.
+ */
 namespace App\Repository;
 
 use App\Entity\Category;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Category>
- *
  * @method Category|null find($id, $lockMode = null, $lockVersion = null)
  * @method Category|null findOneBy(array $criteria, array $orderBy = null)
  * @method Category[]    findAll()
  * @method Category[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ *
+ * @extends ServiceEntityRepository<Category>
  */
 class CategoryRepository extends ServiceEntityRepository
 {
@@ -46,6 +50,7 @@ class CategoryRepository extends ServiceEntityRepository
     public function queryAll(): QueryBuilder
     {
         return $this->getOrCreateQueryBuilder()
+            ->select('partial category.{id, createdAt, updatedAt, title}')
             ->orderBy('category.updatedAt', 'DESC');
     }
 
